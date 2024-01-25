893af02e9fe33392aa529f3024ead72874707270
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2024-01-25 09:34:59 -0300

    feat: Add dotenv e oculta a string de conexão do db

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const userRouter = require('./src/routes/user.route');
 const connectDatabase = require('./src/database/db')
-
+const userRouter = require('./src/routes/user.route');
+require('dotenv').config();
 const app = express();
 connectDatabase();
+
 const port = 3000;
 
 app.use(express.json());
--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDatabase = () => {
     console.log("Connecting to the database...")
 
-    mongoose.connect("mongodb+srv://root:root@cluster0.un3tryp.mongodb.net/?retryWrites=true&w=majority")
+    mongoose.connect(process.env.DB_URI_KEY)
         .then(() => console.log("Connected to MongoDB Atlas database"))
         .catch((error) => console.log(error));
 }
