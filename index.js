2c7da4c1eae6f7bd0abfc8a324546cd47887ffbb
pedroFonseca447 <pedrofonseca447@gmail.com>
2024-01-26 15:28:08 -0300

    primeira tentativa

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ import express from 'express';
 import connectDatabase from './src/database/db.js';
 import userRouter from './src/routes/user.route.js';
 import dotenv from 'dotenv';
+import { getAuth} from 'firebase/auth';
+import User from './src/models/User.js';
+import bodyParser from 'body-parser';
+
+
 dotenv.config();
 const app = express();
 connectDatabase();
 
 const port = 3000;
 
+app.use('/users', userRouter);
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.post('/salvarDados', async (req,res) =>{
+    try{
+        const userProfiles = req.body.userProfiles;
+
+        res.status(200).json({message: 'dados salvos'})
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).json({error: 'erro ao salvar'})
+    }
+});
+
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
