f5139e9d66c2515d9b2c6d8ea59d1707b19c2d30
Pedro B B Fonseca <84049639+PedroFonseca447@users.noreply.github.com>
2024-01-28 12:24:25 -0300

    imagem salva

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import projectController from '../controllers/project.controller.js';
 import { validId } from '../middlewares/global.middlewares.js';
 import upload from '../config/multer.js';
-
+import { body } from 'express-validator';
 
 
 
@@ -10,7 +10,10 @@ const router = express.Router();
 
 router.get("/", projectController.findAllProjects);
 router.get("/:id", validId, projectController.findProjectById);
-router.post("/", upload.single('file') ,projectController.createProject);
+router.post("/", upload.single('file'),
+body('urlGithub').isURL().withMessage("use um link valido")
+
+,projectController.createProject);
 router.patch("/:id", validId, projectController.updateProject);
 router.delete("/:id", validId, projectController.deleteProjectById);
 
